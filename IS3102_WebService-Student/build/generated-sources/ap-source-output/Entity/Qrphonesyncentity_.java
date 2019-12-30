c25db982ad4df12a74303a442f3e7a40796f31d0
Gary Ang <wei_bo99@hotmail.com>
2019-12-30 16:50:15 +0800

    first

---
+++ IS3102_WebService-Student/build/generated-sources/ap-source-output/Entity/Qrphonesyncentity_.java
@@ -0,0 +1,15 @@
+package Entity;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-04-21T16:26:35")
+@StaticMetamodel(Qrphonesyncentity.class)
+public class Qrphonesyncentity_ { 
+
+    public static volatile SingularAttribute<Qrphonesyncentity, String> memberemail;
+    public static volatile SingularAttribute<Qrphonesyncentity, String> qrcode;
+    public static volatile SingularAttribute<Qrphonesyncentity, Long> id;
+
+}
